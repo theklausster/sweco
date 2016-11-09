dbd34941ac8401b004cf678f741d6762e8e24173
Klaus Jakobsen <klaus.jakobsen@non.schneider-electric.com>
2016-11-09 21:44:03 +0100

    -filter by skill added, -search sort on the filter, -reset filter added, -animation and styling added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,7 +11,9 @@ import * as _ from 'lodash';
 export class HomeComponent {
   private contacts = [];
   private activeItem;
-  private filteredContacts;
+  private filterTabs = [];
+  private filteredContactsByTabsTemp = []; // for onSearch Function
+  private filteredContactsByTabs = [];
   
   constructor(private contactService: ContactService) {
 
@@ -19,12 +21,15 @@ export class HomeComponent {
 
   ngOnInit() {
     this.contacts = this.contactService.getContacts();
-    this.filteredContacts = this.contacts; 
+    this.populateFilterTabs();
+    this.addContactByTabs();
+
+   
   
   }
 
   private onSearch(searchInput) {
-      this.filteredContacts = _.filter(this.contacts, (contact: any) => contact.name.toLowerCase().indexOf(searchInput.toLowerCase()) !== -1);
+      this.filteredContactsByTabs = _.filter(this.filteredContactsByTabsTemp, (contact: any) => contact.name.toLowerCase().indexOf(searchInput.toLowerCase()) !== -1);
   }
 
 
@@ -36,9 +41,91 @@ export class HomeComponent {
       else {
         this.activeItem = null;
       }
-      console.log('this.activeitem', this.activeItem);
   }
 
+  // Pushing all skills from all contact into a list
+  // I use lodash to Capitalize every item
+  // I use uniq so it removes duplicates
+  private populateFilterTabs() {
+      _.forEach(this.contacts, (item) => {
+          if (item.skills) {
+            _.forEach(item.skills, (skill) => {
+              if (skill) {
+                this.filterTabs.push(_.capitalize(skill));
+              }
+            });
+          }
+      });
+      this.filterTabs = _.uniq(this.filterTabs)
+  }
+
+  // Looping throgh every contacts in list
+  // Every contact has a [] of skills
+  // If an item is found it wont push again
+  private addContactByTabs() {
+         _.forEach(this.contacts, (contact) => {      
+            _.forEach(contact.skills, (skill) => {       
+                  if (this.filteredContactsByTabs.indexOf(contact) === -1) {
+                    let itemFound = _.find(this.filterTabs, (tab) => {
+                      return tab.toLowerCase() === skill.toLowerCase();
+                    }); 
+                    if(itemFound) {
+                      this.filteredContactsByTabs.push(contact);
+                    }
+                }
+            });
+      });
+      this.filteredContactsByTabsTemp = this.filteredContactsByTabs;
+  }
+
+  // Looping throght each contact with skills
+  // Counting tempcount because i want all tabs to be disabled before the contact is removed from the list
+  // Adding multiple contact if statement is true and deleting them, when not looping. 
+  private removeContactByTabs() {
+    let tempCount = 0;
+    let tempArray = [];
+        _.forEach(this.filteredContactsByTabs, (contact) => {  
+              _.forEach(contact.skills, (skill) => {  
+                  if (this.filterTabs.indexOf(_.capitalize(skill)) === -1)  {
+                        tempCount++;
+                        if (tempCount === contact.skills.length) {
+                            tempArray.push(contact);
+                            tempCount = 0;
+                        }
+                  }
+                  else {
+                       tempCount = 0;
+                  }
+              }); 
+       });
+
+      if(tempArray.length > 0) {
+          _.forEach(tempArray, (itemForDeletion) => {
+              _.remove(this.filteredContactsByTabs, (x) => {
+                      return x === itemForDeletion;
+                });
+          });
+          tempArray = [];
+          this.filteredContactsByTabsTemp = this.filteredContactsByTabs;
+
+      }
+
+}
+
+  // removes a tab 
+  private removeTab(item) {
+      _.remove(this.filterTabs, (tab) => {
+          return tab === item;
+      });
+      this.removeContactByTabs();
+  }
+
+  private resetTabs() {
+    this.filterTabs = [];
+    this.filteredContactsByTabs = [];
+    this.populateFilterTabs();
+    this.addContactByTabs();
+  }
 
  
 }
